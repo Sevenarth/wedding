5814e48b877b09c19f6fa0df877e4ffc81572fda
Luca Vizzarro <me@sowia.me>
2024-12-07 00:18:34 +0000

    add some basic style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script>
     import * as m from "$paraglide/messages";
+    import anagram from "$lib/assets/anagram.png";
 </script>
 
-<h1>{m.heroic_less_jackal_arrive()}</h1>
-<p>
-    {m.super_bold_iguana_link()}
-</p>
+<div class="container mx-auto my-8 px-16 flex flex-col text-center items-center space-x-4">
+    <h1>{m.heroic_less_jackal_arrive()}</h1>
+    <p>
+        {m.super_bold_iguana_link()}
+    </p>
+    <img class="object-contain w-24 mt-8" src={anagram} alt="S&L">
+</div>
