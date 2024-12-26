ff28382ac69c7dc09e643e112130f590a44e1dd5
Luca Vizzarro <me@sowia.me>
2024-12-26 16:10:19 +0100

    add stickiness to story cards
    Signed-off-by: Luca Vizzarro <me@sowia.me>

--- src/lib/Story.svelte
+++ src/lib/Story.svelte
@@ -29,18 +29,18 @@
 
 <section class="mb-5 container mx-auto">
 {#each story as entry, i}
-    <div id={`story-${i+1}`} class="flex flex-col items-center h-lvh w-full snap-start">
-        <div class="flex justify-around items-center w-full grow flex-col lg:flex-row">
-            <div class={`bg-white drop-shadow-xl flex flex-col justify-between w-5/6 lg:w-3/5 grow p-4 pb-10 space-y-4 ${pictureRotations[i % pictureRotations.length]}`}>
+    <div id={`story-${i+1}`} class="sticky top-0 flex flex-col items-center h-lvh w-full snap-start">
+        <div class="flex justify-around items-center w-full grow flex-col md:flex-row">
+            <div class={`bg-white drop-shadow-xl flex flex-col justify-between w-5/6 md:w-3/5 grow p-4 pb-10 space-y-4 ${pictureRotations[i % pictureRotations.length]}`}>
                 <div class="w-full h-full">
                     <img src={entry.image} alt={entry.alt} class="object-cover w-full h-full" />
                 </div>
             </div>
-            <div class="border-t lg:border-l lg:border-t-0 border-white/50 mt-8 mx-8 py-4 lg:m-8 lg:p-6 lg:w-1/3 flex flex-col space-around justify-center">
-                <h2 class="text-4xl md:text-5xl">{entry.title}</h2>
-                <p class="text-xl md:text-2xl">{entry.text}</p>
+            <div class="border-t md:border-l md:border-t-0 border-white/50 mt-8 mx-8 py-4 md:m-8 md:p-6 md:w-1/3 flex flex-col space-around justify-center">
+                <h2 class="text-4xl sm:text-5xl">{entry.title}</h2>
+                <p class="text-xl sm:text-2xl">{entry.text}</p>
             </div>
         </div>
-    </div>
-{/each} 
+    </div >
+{/each}
 </section>
