f899c05272aead5aa7c48780674f4b4f2eca5e90
Luca Vizzarro <me@sowia.me>
2025-02-14 01:12:09 +0000

    fix prisma vite bundling
    Signed-off-by: Luca Vizzarro <me@sowia.me>

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite';
 import { paraglide } from "@inlang/paraglide-vite"
 import Icons from 'unplugin-icons/vite';
 
+// Fix for Prisma Client in the browser. Issue URL: https://github.com/prisma/prisma/issues/12504#issuecomment-1599452566
+import { createRequire } from 'module'
+import path from 'path'
+const { resolve } = createRequire(import.meta.url)
+const prismaClient = `prisma${path.sep}client`
+const prismaClientIndexBrowser = resolve('@prisma/client/index-browser').replace(`@${prismaClient}`, `.${prismaClient}`)
+
+
 export default defineConfig({
 	plugins: [
 		paraglide({
@@ -13,5 +21,11 @@ export default defineConfig({
 		Icons({
 		  compiler: 'svelte',
 		})
-	]
+	],
+	// Fix for Prisma Client in the browser. Issue URL: https://github.com/prisma/prisma/issues/12504#issuecomment-1599452566
+    resolve: {
+        alias: {
+            '.prisma/client/index-browser': path.relative(__dirname, prismaClientIndexBrowser),
+        },
+    },
 });
