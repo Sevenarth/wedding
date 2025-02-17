1c822730f5250760a6d4a05300c53b881a9cbb48
Luca Vizzarro <me@sowia.me>
2025-02-17 01:45:49 +0000

    add our story
    Signed-off-by: Luca Vizzarro <me@sowia.me>

--- src/lib/story.ts
+++ src/lib/story.ts
@@ -1,20 +1,123 @@
+import * as m from "$paraglide/messages";
+import Photo2016 from "$lib/assets/2016.jpg";
+import Photo2017 from "$lib/assets/2017.jpeg";
+import Photo2018Italy from "$lib/assets/2018-italy.jpeg";
+import Photo2018Romania from "$lib/assets/2018-romania.jpeg";
+import Photo2019 from "$lib/assets/2019.jpeg";
+import Photo2019Bonus from "$lib/assets/2019-cameo.jpeg";
+import Photo2020 from "$lib/assets/2020.jpeg";
+import Photo2020Bonus from "$lib/assets/2020-snow.jpeg";
+import Photo2021 from "$lib/assets/2021.jpeg";
+import Photo2021Bonus from "$lib/assets/2021-grad.jpeg";
+import Photo2022 from "$lib/assets/2022.jpeg";
+import Photo2022Bonus from "$lib/assets/2022-grad.jpeg";
+import Photo2023 from "$lib/assets/2023.jpeg";
+import Photo2023Bonus from "$lib/assets/2023-first-black-tie.jpeg";
+import Photo2024 from "$lib/assets/2024.jpeg";
+import Photo2024Bonus from "$lib/assets/2024-wed.jpeg";
+import Photo2025 from "$lib/assets/2025.jpeg";
+
 export default [
     {
-        image: "https://picsum.photos/1600/900/",
-        alt: "Wedding",
-        title: "Our Wedding",
-        text: "We're getting married on the 1st of January 2023. We can't wait to celebrate with you!"
+        image: Photo2016,
+        alt: "Picture of us in 2016",
+        title: m.mellow_candid_turkey_pave(),
+        text: m.factual_same_gazelle_enchant()
     },
     {
-        image: "https://picsum.photos/1600/900/",
-        alt: "Honeymoon",
-        title: "Our Honeymoon",
-        text: "We're going to the Maldives for our honeymoon. We're so excited to relax and enjoy the sun!"
+        image: Photo2017,
+        alt: "Picture of us in 2017",
+        title: m.this_flaky_cobra_pave(),
+        text: m.grand_misty_meerkat_nudge()
     },
     {
-        image: "https://picsum.photos/1600/900/",
-        alt: "Love",
-        title: "Our Love",
-        text: "We met in 2019 and have been inseparable ever since. We can't wait to spend the rest of our lives together!"
-    }
+        image: Photo2018Italy,
+        alt: "Picture of us in 2018",
+        title: "2018 – Puglia",
+        text: m.bright_flaky_sloth_slide()
+    },
+    {
+        image: Photo2018Romania,
+        alt: "Picture of us in 2018",
+        title: m.top_muddy_gadfly_grow(),
+        text: m.muddy_left_newt_sprout()
+    },
+    {
+        image: Photo2019,
+        alt: "Picture of us in 2019",
+        title: m.wild_knotty_millipede_pout(),
+        text: m.clean_wide_bullock_glow()
+    },
+    {
+        image: Photo2019Bonus,
+        alt: "Picture of us in 2019",
+        title: m.mad_red_ox_sprout(),
+        text: m.any_sunny_porpoise_emerge()
+    },
+    {
+        image: Photo2020,
+        alt: "Picture of us in 2020",
+        title: m.spare_sound_earthworm_peek(),
+        text: m.weary_tough_oryx_cure()
+    },
+    {
+        image: Photo2020Bonus,
+        alt: "Picture of us in 2020",
+        title: m.frail_alert_pigeon_fry(),
+        text: m.this_ago_eel_burn()
+    },
+    {
+        image: Photo2021Bonus,
+        alt: "Picture of us in 2021",
+        title: m.dirty_day_tadpole_link(),
+        text: m.sour_front_lemur_charm()
+    },
+    {
+        image: Photo2021,
+        alt: "Picture of us in 2021",
+        title: m.arable_plain_bear_find(),
+        text: m.formal_less_javelina_fall()
+    },
+    {
+        image: Photo2022Bonus,
+        alt: "Picture of us in 2022",
+        title: m.spry_any_rook_view(),
+        text: m.free_still_orangutan_cook()
+    },
+    {
+        image: Photo2022,
+        alt: "Picture of us in 2022",
+        title: m.sweet_grassy_beaver_ripple(),
+        text: m.grand_early_fox_loop()
+    },
+    {
+        image: Photo2023,
+        alt: "Picture of us in 2023",
+        title: "2023 – Tromsø",
+        text: m.fluffy_main_shrimp_dance()
+    },
+    {
+        image: Photo2023Bonus,
+        alt: "Picture of us in 2023",
+        title: m.cute_key_chicken_nail(),
+        text: m.trite_that_weasel_drum()
+    },
+    {
+        image: Photo2024,
+        alt: "Picture of us in 2024",
+        title: m.sour_front_quail_flip(),
+        text: m.every_tense_albatross_chop()
+    },
+    {
+        image: Photo2024Bonus,
+        alt: "Picture of us in 2024",
+        title: m.heavy_stale_ocelot_conquer(),
+        text: m.candid_ornate_jannes_dash()
+    },
+    {
+        image: Photo2025,
+        alt: "Picture of us in 2025",
+        title: m.plane_front_octopus_cook(),
+        text: m.weird_short_felix_cure()
+    },
 ];
